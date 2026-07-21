82151851353a09f8d0a60ffd94e465ed243b41c2
Emanuele Favero <emafaux89@gmail.com>
2026-07-21 18:41:59 +0200

    feat: display author in post list

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -4,7 +4,10 @@ import { Button } from './ui/Button';
 
 export const Post = ({ post, onDeletePost }) => (
   <article className='post'>
-    <h2>{post.title}</h2>
+    <div className='content'>
+      <h2>{post.title}</h2>
+      <p className='author text-sm'>By {post.author}</p>
+    </div>
     <Button
       variant={Button.variant.danger}
       className='delete-button'
